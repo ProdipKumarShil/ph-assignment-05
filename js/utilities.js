ca66f408a3d9da24baaad5a680cd67ca5e81cf6b
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-02-18 11:07:09 +0600

    get value by id function added

--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,15 @@
-
-function randH(s, l){
+// random color
+function randH(s, l){ 
     let h = Math.floor(Math.random() * 360) + 1;
     return 'hsl('+h+', '+s+'%, '+l+'%)';
 }
+
+// getValueById function
+function getValueById(idName){
+    const element = document.getElementById(idName);
+    return element;
+}
+
 
 // document.getElementById('triangle-card').addEventListener('mouseover', function(){
 //     const randCol = randH(100, 68);
