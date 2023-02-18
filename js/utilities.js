943d02738a96a69326a21f0da13238c512fec535
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-02-18 10:57:09 +0600

    added all card

---
+++ js/utilities.js
@@ -0,0 +1,16 @@
+
+function randH(s, l){
+    let h = Math.floor(Math.random() * 360) + 1;
+    return 'hsl('+h+', '+s+'%, '+l+'%)';
+}
+
+// document.getElementById('triangle-card').addEventListener('mouseover', function(){
+//     const randCol = randH(100, 68);
+//     document.getElementById('triangle-card').style.backgroundColor = randCol;
+//     console.log(randCol)
+    
+// })
+
+
+
+
